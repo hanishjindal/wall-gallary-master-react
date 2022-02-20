6dd1b8a5e37dce95fe56b61d1867fb018ae03e2f
Hanish Jindal <hanish1139.cse19@chitkara.edu.in>
2022-02-20 17:18:51 +0530

    fiirst sommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="gallery">
+        <img
+          src="https://images.unsplash.com/photo-1571217668979-f46db8864f75?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="flex-gallery"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1571210059434-edf0dc48e414?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          alt="flex-gallery"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1571030439710-121569126a76?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          alt="flex-gallery"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1569683795645-b62e50fbf103?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=60"
+          alt="flex-gallery"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1569834381156-7b735e41e57d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          alt="flex-gallery"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1569493086584-33e0b36f3145?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          alt="flex-gallery"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1569398034126-476b0d96e2d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          alt="flex-gallery"
+        />
+        <img
+          src="https://images.unsplash.com/flagged/photo-1569430044663-054ffc0c50c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          alt="flex-gallery"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1569241786334-1214c0f1f3bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          alt="flex-gallery"
+        />
+      </div>
     </div>
   );
 }
